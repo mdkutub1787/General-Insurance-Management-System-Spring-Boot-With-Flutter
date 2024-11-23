f77f60bf804646d4974cc7365c985e808e757777
Md Kutub Uddin <158472932+mdkutub1787@users.noreply.github.com>
2024-11-23 15:14:34 +0600

    Update main.dart

--- Flutter/general_insurance_management/lib/main.dart
+++ Flutter/general_insurance_management/lib/main.dart
@@ -12,6 +12,7 @@ import 'package:general_insurance_management/Reports/Total_Marine_Bill_Report.da
 import 'package:general_insurance_management/marinepolicy/view_maeine_bill.dart';
 import 'package:general_insurance_management/marinepolicy/view_marine_money_receipt.dart';
 import 'package:general_insurance_management/marinepolicy/view_marine_policy.dart';
+import 'package:general_insurance_management/page/Head_Office.dart';
 import 'package:general_insurance_management/page/Home.dart';
 import 'package:general_insurance_management/page/login.dart';
 import 'package:general_insurance_management/page/registration.dart';
@@ -61,7 +62,7 @@ class _MyAppState extends State<MyApp> {
               child: Text("The page you're looking for doesn't exist.")),
         ),
       ),
-      home: Login(),
+      home: HeadOffice(),
     );
   }
 }

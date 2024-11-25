0b421800e71bdfbfaffe0dc65907bd138d6dda99
Md Kutub Uddin <158472932+mdkutub1787@users.noreply.github.com>
2024-11-25 17:47:50 +0600

    Update MarineInsuranceBillRepo.java

--- Spring-Boot/InsuranceManagement/src/main/java/com/kutub/InsuranceManagement/repository/MarineInsuranceBillRepo.java
+++ Spring-Boot/InsuranceManagement/src/main/java/com/kutub/InsuranceManagement/repository/MarineInsuranceBillRepo.java
@@ -2,9 +2,17 @@ package com.kutub.InsuranceManagement.repository;
 
 import com.kutub.InsuranceManagement.entity.MarineInsuranceBill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MarineInsuranceBillRepo extends JpaRepository<MarineInsuranceBill,Long> {
 
+
+    // Custom query to find bills by policy ID
+    @Query("SELECT b FROM MarineInsuranceBill b WHERE b.marineDetails.id = :marineDetailsId")
+    List<MarineInsuranceBill> findMarineBillsByMarinePolicyId(@Param("marineDetailsId") long marineDetailsId);
 }
